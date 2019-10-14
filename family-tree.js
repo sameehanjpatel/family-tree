fc44e2f2513014df6f7f3e4fc26a0134c09d5ba2
Sameehan Patel <sp4315@nyu.edu>
2019-10-13 21:14:37 -0400

    need to comeplete log

--- family-tree.js
+++ family-tree.js
@@ -1,5 +1,32 @@
 class FamilyTree {
-  constructor () {}
+  constructor (lastName) {
+    super(lastName)
+    this.lastName = lastName
+    this.familyNames = []
+  }
+
+  insert(name) {
+    let family = new FamilyTree(name)
+    this.name.push(name)
+  }
+
+  familySize(){
+    return this.familyNames.length + 1
+  }
+
+  findMember(searchQuery){
+    return this.familyNames.filter(searchName => {
+      if(searchName === searchQuery){
+        return searchName;
+      }
+    })
+  }
+
+  log(){
+  }
+
+  
+
 }
 
 module.exports = FamilyTree;
